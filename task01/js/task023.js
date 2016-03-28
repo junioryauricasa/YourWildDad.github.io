50cbd978d8a0c6297d7b6d03be98e9600f0fbf3a
YourWildDad <053700@gmail.com>
2016-03-28 08:50:43 +0800

    百度前端技术学院第二批任务 2016-3-28 commit by YourWildDad task023 v0.2.0

--- task01/js/task023.js
+++ task01/js/task023.js
@@ -2,74 +2,70 @@
  * Created by YourWildDad on 2016/3/24.
  */
 var interval;
-var changeBkColor = function (divList) {
-    var length = divList.length, i = 0;
-    divList[i++].style.backgroundColor = "blue";
-    interval = setInterval(function () {
-        if (i < length) {
-            divList[i - 1].style.backgroundColor = '#fff';
-            console.log("当前遍历的div:" + divList[i].firstChild.nodeValue);
-            divList[i++].style.backgroundColor = "blue";
+var changeBkColor = function(divList) {
+	var length = divList.length,
+		i = 0;
+	divList[i++].style.backgroundColor = "blue";
+	interval = setInterval(function() {
+		if (i < length) {
+			divList[i - 1].style.backgroundColor = '#fff';
+			console.log("当前遍历的div:" + divList[i].firstChild.nodeValue);
+			divList[i++].style.backgroundColor = "blue";
 
-        } else {
-            clearInterval(interval);
-            divList[i - 1].style.backgroundColor = '#fff';
-        }
-    }, document.getElementById('set-time').value)
+		} else {
+			clearInterval(interval);
+			divList[i - 1].style.backgroundColor = '#fff';
+		}
+	}, document.getElementById('set-time').value)
 };
-var traversal = function (type) {
-    var divList = [];
-    reset();
-    var root = document.getElementById("root");
-    switch (type) {
-        case 'pre':
-            pre_traversal(root, divList);
-            break;
-        case 'in':
-            in_traversal(root, divList);
-            break;
-        case 'post':
-            post_traversal(root, divList);
-            break;
-        default:
-    }
-    changeBkColor(divList);
+var traversal = function(type) {
+	var divList = [];
+	reset();
+	var root = document.getElementById("root");
+	switch (type) {
+		case 'pre':
+			pre_traversal(root, divList);
+			break;
+		case 'in':
+			in_traversal(root, divList);
+			break;
+		case 'post':
+			post_traversal(root, divList);
+			break;
+		default:
+	}
+	changeBkColor(divList);
 };
-var pre_traversal = function (node, divList) {
-    var zhan = [];
-    if (!node) {
-        console.log("这个节点是空的");
-    } else {
-        while (node) {
-            while (node) {    //只要结点不为空就应该入栈保存，与其左右结点无关
-                divList.push(node);
-                zhan.push(node);
-                node = node.firstElementChild;
-            }
-            node = zhan.pop();
-            node = node.lastElementChild;
-        }
-    }
+var pre_traversal = function(node, divList) {
+	divList.push(node);
+	var children = node.children,
+		length = children.length;
+	for (var i = 0; i < length; i++) {
+		pre_traversal(children[i], divList);
+	}
 };
 
-var in_traversal = function (node, divList) {
-    if (node) {
-        in_traversal(node.firstElementChild, divList);
-        divList.push(node);
-        in_traversal(node.lastElementChild, divList);
-    }
+var in_traversal = function(node, divList) {
+	var children = node.children,
+		length = children.length;
+	for (var i = 0; i < length; i++) {
+		divList.push(children[i]);
+		pre_traversal(children[i], divList);
+	}
+
 };
-var post_traversal = function (node, divList) {
-    if (node) {
-        post_traversal(node.firstElementChild, divList);
-        post_traversal(node.lastElementChild, divList);
-        divList.push(node);
-    }
+var post_traversal = function(node, divList) {
+	var children = node.children,
+		length = children.length;
+	for (var i = 0; i < length; i++) {
+		pre_traversal(children[i], divList);
+	}
+	divList.push(node);
 };
-var reset = function () {
-    clearInterval(interval);
-    var divs = document.getElementsByTagName('div');
-    for (var i = divs.length - 1; i >= 0; i--) {
-        divs[i].style.backgroundColor = '#fff';
-    }
+var reset = function() {
+	clearInterval(interval);
+	var divs = document.getElementsByTagName('div');
+	for (var i = divs.length - 1; i >= 0; i--) {
+		divs[i].style.backgroundColor = '#fff';
+	}
 };
