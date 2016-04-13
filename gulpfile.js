cb0c8ef3d893b430ac652c21f34b40639147c4a9
YourWildDad <053700@gmail.com>
2016-04-13 10:27:11 +0800

    新建任务
    新建任务

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,31 @@
 'use strict';
-var gulp = require('gulp'),
+const gulp = require('gulp'),
     less = require('gulp-less'),
     LessPluginAutoPrefix = require("less-plugin-autoprefix"),
+    babel = require('gulp-babel'),
     autoprefix = new LessPluginAutoPrefix({
         browsers: [
             'last 4 versions',
             "ie >= 7"
         ]
     });
-gulp.task('testLess', function() {
-    gulp.src('./task00/style/*.less')
-        .pipe(less({plugins: [autoprefix]}))
+gulp.task('Less', () => {
+    return gulp.src('./task00/style/*.less')
+        .pipe(less({
+            plugins: [autoprefix]
+        }))
         .pipe(gulp.dest('./task00/style'));
 });
-gulp.task('testWatch', function() {
+gulp.task('babel', () => {
+    return gulp.src('./task02/*.es6')
+        .pipe(babel({
+            presets: ['es2015']
+        }))
+        .pipe(gulp.dest('./task02'));
+})
+gulp.task('watch', function() {
     gulp.watch('./task00/style/*.less', ['testLess']);
+    gulp.watch('./task02/*.es6', ['babel']);
 });
-gulp.task('less', ['testLess', 'testWatch']);
+gulp.task('dev', ['Less', 'babel', 'watch']);
 gulp.task('default', []);
---
+++ task02/modal.js
@@ -0,0 +1,7 @@
+import $ from 'jQuery';
+export class xsc_modal {
+    constructor(ele, options) {
+        $.extend(this.options, options);
+
+    }
+}
